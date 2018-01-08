0efeb39dc50f20a29c28365f2e185b156c746e58
FixYouZi <35181810+FixYouZi@users.noreply.github.com>
2018-01-08 18:50:07 +0800

    123
    123

---
+++ www/test1.py
@@ -0,0 +1,15 @@
+import sys
+import orm,asyncio
+from models import User,Blog,Comment
+
+
+async def test(loop,**kw):
+    await orm.create_pool(loop=loop,user='www-data', password='www-data', db='awesome')
+    u = User(name=kw.get('name'), email=kw.get('email'), passwd=kw.get('passwd'), image=kw.get('image'))
+    await u.save()
+    await orm.destory_pool()
+
+data=dict(name='gaf', email='235123345@qq.com', passwd='1312345', image='about:blank')
+loop=asyncio.get_event_loop()
+loop.run_until_complete(test(loop,**data))
+loop.close()
